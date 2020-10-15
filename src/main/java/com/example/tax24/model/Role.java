c5414cbe82e10dc83383dcfe88cbbc2567f1305d
frankhn <harfrank2@gmail.com>
2020-10-15 18:05:05 +0200

    adds role model

--- src/main/java/com/example/tax24/model/Role.java
+++ src/main/java/com/example/tax24/model/Role.java
@@ -1,8 +1,47 @@
 package com.example.tax24.model;
 
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
-public class Location {
+public class Role {
     @PrimaryKeyJoinColumn
     private long id;
+
+    public long name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId", nullable=false)
+    public User userId;
+
+    public Role(long id, long name, User userId) {
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getName() {
+        return name;
+    }
+
+    public void setName(long name) {
+        this.name = name;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
 }
