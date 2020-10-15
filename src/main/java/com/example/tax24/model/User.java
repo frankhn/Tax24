77056526f7a4297c1868be2afd12591a2c3ba0ff
frankhn <harfrank2@gmail.com>
2020-10-15 20:05:39 +0200

    adds user roles

---
+++ src/main/java/com/example/tax24/model/User.java
@@ -0,0 +1,81 @@
+package com.example.tax24.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@PrimaryKeyJoinColumn
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	private String firstName;
+
+	@NotNull
+	private String lastName;
+
+	@OneToMany(mappedBy = "userid", fetch = FetchType.LAZY,
+			cascade = CascadeType.ALL)
+	private Set<Role> roles;
+
+	@NotNull
+	@Email
+	@NotBlank
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	public User() {
+
+	}
+
+	public User(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
