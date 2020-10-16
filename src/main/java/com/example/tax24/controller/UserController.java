4b598c4fc0fac8d8d81597d9fd2e94bb71b01cac
frankhn <harfrank2@gmail.com>
2020-10-16 04:52:16 +0200

    removes unused import statements

--- src/main/java/com/example/tax24/controller/UserController.java
+++ src/main/java/com/example/tax24/controller/UserController.java
@@ -1,27 +1,17 @@
 package com.example.tax24.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.persistence.ElementCollection;
 import javax.validation.Valid;
 
 import com.example.tax24.exception.ResourceNotFoundException;
 
 import com.example.tax24.repository.RoleRepository;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +32,14 @@ public class UserController {
 	@GetMapping("/drivers")
 	public List<User> findByRoleName() {
 		return userRepository.findByRoles_name("user");
+	}
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId)
+			throws ResourceNotFoundException {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found"));
+		return ResponseEntity.ok().body(user);
 	}
 
 	@PostMapping("/users")
