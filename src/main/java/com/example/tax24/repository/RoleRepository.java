a6e578d750ad4a3d1c647f3488ecc72a11e9426e
frankhn <harfrank2@gmail.com>
2020-10-16 05:15:48 +0200

    adds role repository interface

--- src/main/java/com/example/tax24/repository/RoleRepository.java
+++ src/main/java/com/example/tax24/repository/RoleRepository.java
@@ -1,2 +1,11 @@
-package com.example.tax24.repository;public class RoleRepository {
+package com.example.tax24.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.tax24.model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
 }
