1d088fa3859146ee6e1604c2ac8cd9baf7ba0933
frankhn <harfrank2@gmail.com>
2021-02-08 08:25:21 +0200

    adds seeds to the db

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,32 @@
+INSERT INTO users(id, name, email)
+VALUES(1, 'Eric manuella', 'email1@tax24.com');
+
+INSERT INTO users(id, name, email)
+VALUES(2, 'Munana James', 'james@tax24.com');
+
+INSERT INTO users(id, name, email)
+VALUES(3, 'Kamali passcy', 'kamali@tax24.com');
+
+INSERT INTO users(id, name, email)
+VALUES(4, 'Kamaliza jacky', 'kamaliza@tax24.com');
+
+
+INSERT INTO roles(id, name, user_id)
+VALUES(1, 'user', 1);
+
+INSERT INTO roles(id, name, user_id)
+VALUES(2, 'user', 2)
+
+INSERT INTO roles(id, name, user_id)
+VALUES(3, 'user', 3)
+
+INSERT INTO roles(id, name, user_id)
+VALUES(4, 'user', 4)
+
+
+--Drivers
+INSERT INTO roles(id, name, user_id)
+VALUES(7, 'driver', 3)
+
+INSERT INTO roles(id, name, user_id)
+VALUES(8, 'driver', 1)
